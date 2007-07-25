df93b82288026809cea6a8c99d0fb505ae232cec
Greg Sabino Mullane <greg@users.mediawiki.org>
2007-07-25 14:34:26 +0000

    Add primary key, fix errant commas

--- ConfirmAccount.pg.sql
+++ ConfirmAccount.pg.sql
@@ -1,13 +1,12 @@
 -- (c) Aaron Schulz, 2007
 
 -- Table structure for table `Confirm account`
--- Replace /*$wgDBprefix*/ with the proper prefix
 
 BEGIN;
 
 CREATE SEQUENCE account_requests_acr_id_seq;
 CREATE TABLE account_requests (
-  acr_id                   INTEGER  NOT NULL DEFAULT nextval('account_requests_acr_id_seq'),
+  acr_id                   INTEGER  PRIMARY KEY NOT NULL DEFAULT nextval('account_requests_acr_id_seq'),
   acr_name                 TEXT     NOT NULL UNIQUE,
   acr_real_name            TEXT,
   acr_email                TEXT,
@@ -20,10 +19,10 @@ CREATE TABLE account_requests (
   acr_url                  TEXT,
   acr_ip                   CIDR,
   acr_rejected             TIMESTAMPTZ,
-  acr_user                 INTEGER                REFERENCES mwuser(user_id) ON DELETE SET NULL,
+  acr_user                 INTEGER                REFERENCES mwuser(user_id) ON DELETE SET NULL
 );
 
-CREATE INDEX acr_rejected_reg ON account_requests (acr_rejected,acr_registration),
+CREATE INDEX acr_rejected_reg ON account_requests (acr_rejected,acr_registration);
 CREATE INDEX acr_email_token ON account_requests (acr_email_token);
 
 COMMIT;
