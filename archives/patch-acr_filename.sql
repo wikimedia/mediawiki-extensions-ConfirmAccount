b0b546a8c45616db6858bbcbad3967de632a103f
Aaron Schulz <aaron@users.mediawiki.org>
2008-01-06 19:20:33 +0000

    Fix typo

--- archives/patch-acr_filename.sql
+++ archives/patch-acr_filename.sql
@@ -4,4 +4,4 @@ ALTER TABLE /*$wgDBprefix*/account_requests
 	ADD acr_filename VARCHAR(255) NULL,
     ADD acr_storage_key VARCHAR(64) NULL,
     ADD acr_held binary(14),
-    ADD acr_reason VARCHAR(255) NULL;
+    ADD acr_comment VARCHAR(255) NULL;
