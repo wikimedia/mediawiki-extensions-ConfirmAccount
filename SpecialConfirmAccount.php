d78c1facc6e5d0174d180cb833f654555ad6edc1
Aaron Schulz <aaron@users.mediawiki.org>
2008-03-07 15:24:07 +0000

    Bump

--- SpecialConfirmAccount.php
+++ SpecialConfirmAccount.php
@@ -12,7 +12,7 @@ $wgExtensionCredits['specialpage'][] = array(
 	'descriptionmsg' => 'confirmedit-desc',
 	'author' => 'Aaron Schulz',
 	'url' => 'http://www.mediawiki.org/wiki/Extension:ConfirmAccount',
-	'version' => '1.4',
+	'version' => '1.41',
 );
 
 # This extension needs email enabled!
