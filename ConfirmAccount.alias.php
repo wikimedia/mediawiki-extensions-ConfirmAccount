c06117f7c3da9e55383e0bb2ba20722b0650d743
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-08-07 23:29:59 +0000

    * Fix a lot of extension special pages mess. Less messy now, aside from the naming of the files containing the aliases. That's for next time. * Add special page aliases for AbuseFilter * Change special page aliases method for ChangeAuthor, and Collection * Add missing to Translate/aliases.txt
    (may require another commit; we will see)

--- ConfirmAccount.alias.php
+++ ConfirmAccount.alias.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Aliases for Special:ConfirmAccount
+ * Aliases for extension ConfirmAccount
  *
  * @addtogroup Extensions
  */
@@ -11,99 +11,100 @@ $aliases = array();
  * @author Jon Harald Søby
  */
 $aliases['en'] = array(
-    'ConfirmAccount' => array( 'ConfirmAccount' ),
+	'RequestAccount'  => array( 'RequestAccount' ),
+	'ConfirmAccounts' => array( 'ConfirmAccounts' ),
+	'UserCredentials' => array( 'UserCredentials' ),
 );
 
 /** Arabic (العربية)
  * @author Meno25
  */
 $aliases['ar'] = array(
-	'ConfirmAccount' => array( 'تأكيد_الحساب' ),
+	'ConfirmAccounts' => array( 'تأكيد_الحساب' ),
 );
 
 /** Egyptian Spoken Arabic (مصرى)
  * @author Meno25
  */
 $aliases['arz'] = array(
-	'ConfirmAccount' => array( 'تأكيد_الحساب' ),
+	'ConfirmAccounts' => array( 'تأكيد_الحساب' ),
 );
 
 /** Finnish (Suomi) */
 $aliases['fi'] = array(
-	'ConfirmAccount' => array( 'Varmista käyttäjätunnus' ),
+	'ConfirmAccounts' => array( 'Varmista käyttäjätunnus' ),
 );
 
 /** French (Français) */
 $aliases['fr'] = array(
-	'ConfirmAccount' => array( 'ConfirmerCompte', 'Confirmation compte', 'ConfirmationCompte' ),
+	'ConfirmAccounts' => array( 'ConfirmerCompte', 'Confirmation compte', 'ConfirmationCompte' ),
 );
 
 /** Galician (Galego) */
 $aliases['gl'] = array(
-	'ConfirmAccount' => array( 'Confirmar a conta' ),
+	'ConfirmAccounts' => array( 'Confirmar a conta' ),
 );
 
 /** Hebrew (עברית)
  * @author Rotem Liss
  */
 $aliases['he'] = array(
-	'ConfirmAccount' => array( 'אישור_חשבון' ),
+	'ConfirmAccounts' => array( 'אישור_חשבון' ),
 );
 
 /** Croatian (Hrvatski) */
 $aliases['hr'] = array(
-	'ConfirmAccount' => array( 'Potvrdi_račun' ),
+	'ConfirmAccounts' => array( 'Potvrdi_račun' ),
 );
 
 /** Haitian (Kreyòl ayisyen) */
 $aliases['ht'] = array(
-	'ConfirmAccount' => array( 'KonfimeKonpt' ),
+	'ConfirmAccounts' => array( 'KonfimeKonpt' ),
 );
 
 /** Hungarian (Magyar) */
 $aliases['hu'] = array(
-	'ConfirmAccount' => array( 'Felhasználói fiók megerősítése' ),
+	'ConfirmAccounts' => array( 'Felhasználói fiók megerősítése' ),
 );
 
 /** Luxembourgish (Lëtzebuergesch) */
 $aliases['lb'] = array(
-	'ConfirmAccount' => array( 'Benotzerkont confirméieren' ),
+	'ConfirmAccounts' => array( 'Benotzerkont confirméieren' ),
 );
 
 /** Malay (Bahasa Melayu) */
 $aliases['ms'] = array(
-	'ConfirmAccount' => array( 'Sahkan_akaun', 'Mengesahkan_akaun' ),
+	'ConfirmAccounts' => array( 'Sahkan_akaun', 'Mengesahkan_akaun' ),
 );
 
 /** Dutch (Nederlands) */
 $aliases['nl'] = array(
-	'ConfirmAccount' => array( 'GebruikerBevestigen' ),
+	'ConfirmAccounts' => array( 'GebruikerBevestigen' ),
 );
 
 /** Norwegian (bokmål)‬ (‪Norsk (bokmål)‬)
  * @author Jon Harald Søby
  */
 $aliases['no'] = array(
-	'ConfirmAccount' => array( 'Bekreft_konto' ),
+	'ConfirmAccounts' => array( 'Bekreft_konto' ),
 );
 
 /** Portuguese (Português) */
 $aliases['pt'] = array(
-	'ConfirmAccount' => array( 'Confirmar conta' ),
+	'ConfirmAccounts' => array( 'Confirmar conta' ),
 );
 
 /** Brazilian Portuguese (Português do Brasil) */
 $aliases['pt-br'] = array(
-	'ConfirmAccount' => array( 'Confirmar conta' ),
+	'ConfirmAccounts' => array( 'Confirmar conta' ),
 );
 
 /** Swedish (Svenska) */
 $aliases['sv'] = array(
-	'ConfirmAccount' => array( 'Bekräfta konto' ),
+	'ConfirmAccounts' => array( 'Bekräfta konto' ),
 );
 
 /** Thai (ไทย) */
 $aliases['th'] = array(
-	'ConfirmAccount' => array( 'ยืนยันบัญชีผู้ใช้' ),
+	'ConfirmAccounts' => array( 'ยืนยันบัญชีผู้ใช้' ),
 );
-
