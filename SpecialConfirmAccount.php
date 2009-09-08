ce3aa3a638b27887f75e25309622acc75771810c
Brion Vibber <brion@users.mediawiki.org>
2009-09-08 18:40:05 +0000

    Revert broken rewrite of login forms: r55990 "* Totally refactor includes/specials/SpecialUserlogin.php:" r55991 "Followup to r55990 - Remove debugging hack :(" r55994 "Followup to r55990 - rename method to avoid PHP Strict warnings" r55997 "Follow-up to r55990, per comments:"
    Continued reports of breakage with the new version...
    Non-vital changes of this magnitude should probably be worked up on a branch to ensure everything's consistent before pushing them to trunk.

--- SpecialConfirmAccount.php
+++ SpecialConfirmAccount.php
@@ -125,11 +125,11 @@ $dir = dirname( __FILE__ ) . '/';
 $wgExtensionMessagesFiles['ConfirmAccount'] = $dir . 'ConfirmAccount.i18n.php';
 $wgExtensionAliasesFiles['ConfirmAccount'] = $dir . 'ConfirmAccount.alias.php';
 
-function efAddRequestLoginText( &$sp ) {
+function efAddRequestLoginText( &$template ) {
 	global $wgUser;
 	wfLoadExtensionMessages( 'ConfirmAccount' );
 	if ( !$wgUser->isAllowed( 'createaccount' ) ) {
-		$sp->mFormHeader .= wfMsgExt( 'requestaccount-loginnotice', array( 'parse' ) );
+		$template->set( 'header', wfMsgExt( 'requestaccount-loginnotice', array( 'parse' ) ) );
 	}
 	return true;
 }
