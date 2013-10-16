a78546b09d822b7ec9449ba7aef92e253759d34c
paladox <thomasmulhall410@yahoo.com>
2013-10-16 06:43:51 -0400

    Fix sql problem in acr_email
    * This fixes a sql problem in acr_email problem described for mysql at http://www.mydigitallife.info/mysql-error-1170-42000-blobtext-column-used-in-key-specification-without-a-key-length/
    
    Bug: T84999
    Change-Id: Id2067e9fbee4f5b4952b8281e5d6c53a5e85d9fe

---
+++ backend/schema/mysql/patch-alter-acr_email-index.sql
@@ -0,0 +1,3 @@
+-- (c) Aaron Schulz, 2015
+
+ALTER TABLE /*_*/account_requests MODIFY /*i*/acr_email VARCHAR(255) binary NOT NULL;
--- backend/schema/mysql/patch-email-index.sql
+++ backend/schema/mysql/patch-email-index.sql
@@ -1,4 +1,3 @@
 -- (c) Aaron Schulz, 2007
 
-ALTER TABLE /*$wgDBprefix*/account_requests
-	ADD UNIQUE INDEX acr_email(acr_email(255));
+ALTER TABLE /*_*/account_requests ADD INDEX acr_email(acr_email(255));
