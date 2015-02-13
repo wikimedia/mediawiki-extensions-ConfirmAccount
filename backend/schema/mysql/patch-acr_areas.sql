18e26171d6e8d6015cb0e5697c710585b2371792
paladox <thomasmulhall410@yahoo.com>
2015-02-13 18:42:22 +0000

    Missing support here for sqlite
    * Some .sql were missing sqlite support.
    
    * Converts spaces to tabs in .sql file.
    
    Change-Id: I968b0b88233763e80ab9e1aa439518ceb253cc5d

--- backend/schema/mysql/patch-acr_areas.sql
+++ backend/schema/mysql/patch-acr_areas.sql
@@ -1,7 +1,5 @@
 -- (c) Aaron Schulz, 2007
 
-ALTER TABLE /*$wgDBprefix*/account_requests
-	ADD acr_areas mediumblob NOT NULL;
+ALTER TABLE /*_*/account_requests ADD acr_areas mediumblob NOT NULL;
 
-ALTER TABLE /*$wgDBprefix*/account_credentials
-	ADD acd_areas mediumblob NOT NULL;
+ALTER TABLE /*_*/account_credentials ADD acd_areas mediumblob NOT NULL;
