62b914904394b19c6121303e0aabdc9f36191b41
Umherirrender <umherirrender_de.wp@web.de>
2017-11-10 20:23:05 +0100

    build: Always exclude vendor and node_modules
    Change-Id: I350edf543dacf98084162ee9761c1b7ee02427b5

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,8 @@ module.exports = function ( grunt ) {
 			},
 			all: [
 				'**/*.js',
-				'!node_modules/**'
+				'!node_modules/**',
+				'!vendor/**'
 			]
 		},
 		jscs: {
@@ -24,7 +25,8 @@ module.exports = function ( grunt ) {
 		jsonlint: {
 			all: [
 				'**/*.json',
-				'!node_modules/**'
+				'!node_modules/**',
+				'!vendor/**'
 			]
 		}
 	} );
